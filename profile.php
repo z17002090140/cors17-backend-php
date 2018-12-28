8c54844df9b81fcebe63c2a3cb936dc41a831090
Administrator <1>
2018-12-28 08:17:58 +0800

    1

---
+++ profile.php
@@ -0,0 +1,6 @@
+<?php
+header("Access-Control-Allow-Origin:http://localhost:63342");
+header("Access-Control-Allow-Credentials:true");
+$userName=array("userName"=>'123456');
+echo json_encode(userName);
+?>
