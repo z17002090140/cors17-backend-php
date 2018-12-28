a0a8963ec6c85acb54b3fd660c7b66129eaac535
Administrator <1>
2018-12-28 08:47:46 +0800

    2

--- profile.php
+++ profile.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin:http://10.30.29.92:63341");
+header("Access-Control-Allow-Origin:http://10.30.29.92:63341");//前端ip和端口
 header("Access-Control-Allow-Credentials:true");
 header('Access-Control-Allow-Headers:Content-Type');
 $userName=array("userName"=>'123456');
